49f5a6681d3fdd4f837fa2901585cc13ef9406a4
user42 <mvo-van-@student.42.fr>
2021-11-09 22:18:27 +0100

    add pair plot

--- pair_plot.py
+++ pair_plot.py
@@ -3,10 +3,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-lstFeature = ["Arithmancy","Astronomy","Herbology","Defense Against the Dark Arts","Divination","Muggle Studies","Ancient Runes","History of Magic","Transfiguration","Potions","Care of Magical Creatures","Charms","Flying"]
+lstFeature = ["Herbology","Defense Against the Dark Arts","Divination","Muggle Studies","Ancient Runes","History of Magic","Transfiguration","Potions","Charms","Flying"]
+# lstFeature = ["Arithmancy","Astronomy","Herbology","Defense Against the Dark Arts","Divination","Muggle Studies","Ancient Runes","History of Magic","Transfiguration","Potions","Care of Magical Creatures"]
+# lstFeature = ["Charms","Flying"]
+# lstFeature = ["Arithmancy","Astronomy"]
 
 dataSet = pandas.read_csv("datasets/dataset_train.csv", usecols=["Hogwarts House"]+lstFeature)
-dataSet.fillna("",inplace=True)
+# dataSet.fillna(0,inplace=True)
+dataSet = dataSet.dropna(axis=0)
+print(dataSet)
 
-sns_plot = sns.pairplot(dataSet, hue = "Hogwarts House")
-sns_plot = 
+pairplot = sns.pairplot(dataSet,vars=lstFeature, hue = "Hogwarts House", corner=True)
+
+plt.show()
